fix: nan loss by fp16->fp32 (overflow/underflow) and clip_grad_norm_

CLIP.py
@@ -39,7 +39,7 @@ class CLIPModel(nn.Module):
         # import datetime
         # debug_dir = Path("logs" ) /  CFG.experiment_name / "debug_loss"
         # debug_dir.mkdir(exist_ok=True)
-        sns.heatmap(logits.detach().cpu().numpy())
+        ax = sns.heatmap(logits.detach().cpu().numpy())
         sns_figure = ax.get_figure()
         buf = io.BytesIO()
         sns_figure.savefig(buf, format='png')
@@ -94,9 +94,11 @@ class CLIPModel(nn.Module):
         # Getting Image and Text Embeddings (with same dimension)
         image_embeddings1 = self.image_projection1(image_features1)
         image_embeddings2 = self.image_projection2(image_features2)
+        # assert image_embeddings1.shape == image_embeddings2.shape, f"{image_embeddings1.shape} != {image_embeddings2.shape}"
+        # assert len(image_embeddings1.shape) == 2, f"Expected 2D tensor, got {len(image_embeddings1.shape)}D tensor ({image_embeddings1.shape})"
 
-        image_embeddings1 = F.normalize(image_embeddings1, p=2, dim=-1)
-        image_embeddings2 = F.normalize(image_embeddings2, p=2, dim=-1)
+        # image_embeddings1 = F.normalize(image_embeddings1, p=2, dim=1)
+        # image_embeddings2 = F.normalize(image_embeddings2, p=2, dim=1)
 
         # Calculating the Loss
         logits = ((image_embeddings2 @ image_embeddings1.T) + (image_embeddings1 @ image_embeddings2.T)) / 2.0 / self.temperature
@@ -108,6 +110,25 @@ class CLIPModel(nn.Module):
         batch_size = image_embeddings1.shape[0]
         loss = nn.CrossEntropyLoss()(logits, torch.arange(batch_size, device=CFG.device)) # use a simpler loss function 
         # TODO: Check if loss is NaN or inf or -inf
+        if torch.isnan(loss).any():
+            print("Loss is NaN")
+            print("Image Embeddings 1:", image_embeddings1)
+            print("Image Embeddings 2:", image_embeddings2)
+            print("Logits:", logits)
+            print("Cross Entropy Loss:", loss)
+            torch.save({
+                "image_embeddings1": image_embeddings1,
+                "image_embeddings2": image_embeddings2,
+                "logits": logits,
+                "loss": loss,
+                "image1": image1,
+                "image2": image2,
+                "image_encoder1": self.image_encoder1,
+                "image_encoder2": self.image_encoder2,
+                "image_projection1": self.image_projection1,
+                "image_projection2": self.image_projection
+            }, "nan_loss_details.pth")
+            # raise ValueError("Loss is NaN")
         return loss.mean(), F.softmax(logits, dim=-1).detach().cpu()
 
 

config.py
@@ -1,18 +1,20 @@
 import torch
 
-experiment_name="CLIP"
+experiment_name="CLIP_PreIVL_PostStent"
 debug = False
 
 visualize_every = 10
 captions_path = "."
+skip_data_check = False
 batch_size = 16
 num_workers = 18
-lr = 1e-3
+lr = 1e-4
 weight_decay = 1e-3
 patience = 2
 factor = 0.5
 epochs = 100
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+max_norm = 1.0
 
 image_embedding = 1120
 

main.py
@@ -38,7 +38,7 @@ def seed_everything(seed):
     torch.backends.cudnn.benchmark = False
 
 def make_train_valid_dfs():
-    dataframe = pd.read_csv(f"{CFG.captions_path}/pre_final_coregistration.csv")
+    dataframe = pd.read_csv(f"{CFG.captions_path}")
     max_id = dataframe["id"].max() + 1 if not CFG.debug else 100
     image_ids = np.arange(0, max_id)
     np.random.seed(42)
@@ -58,6 +58,7 @@ def build_loaders(dataframe, mode):
         image_filenames1=dataframe["image1"].values,
         image_filenames2=dataframe["image2"].values,
         transforms=transforms,
+        skip_data_check=CFG.skip_data_check,
     )
     # dataset = CLIPDataset(
     #     dataframe["image1"].values,
@@ -83,7 +84,7 @@ def train_epoch(model, train_loader, optimizer, lr_scheduler, step, scaler, epoc
         device = CFG.device
         batch['image1'] = batch['image1'][tio.DATA].permute(0, 1, 4, 3, 2).to(device)
         batch['image2'] = batch['image2'][tio.DATA].permute(0, 1, 4, 3, 2).to(device)
-        with torch.autocast(device_type="cuda", dtype=torch.float16):
+        with torch.autocast(device_type="cuda", dtype=torch.float32):
             visualize = iteration % CFG.visualize_every == 0
             loss, softmax_logit = model(batch, visualize=visualize, epoch=epoch)
             if loss.isnan():
@@ -92,6 +93,8 @@ def train_epoch(model, train_loader, optimizer, lr_scheduler, step, scaler, epoc
             softmax_logits.append(softmax_logit)
         
         scaler.scale(loss).backward()
+        scaler.unscale_(optimizer)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=CFG.max_norm, norm_type=2)
         scaler.step(optimizer)
         scaler.update()
         if step == "batch":